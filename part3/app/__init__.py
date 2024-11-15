2f0eeddedc48d5d0a9779938bb55357033f0c5ef
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-15 09:17:52 +0000

    updating file for task 1 and 2

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,8 +4,11 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as place_ns
+from app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
+jwt = JWTManager()
 bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -13,10 +16,12 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
     api.add_namespace(place_ns, "/api/v1/places")
+    api.add_namespace(auth_ns, "/api/v1/auth")
     return app
