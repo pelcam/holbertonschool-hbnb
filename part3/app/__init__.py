8500c2972470baa404861dcc6d1cc5181838bd4a
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-18 10:19:52 +0000

    updating __init__ files

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -17,13 +17,11 @@ def create_app(config_class=config.DevelopmentConfig):
     bcrypt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     jwt.init_app(app)
-    db.init_app(app)
 
     from .services import HBnBFacade
     globals()["facade"] = HBnBFacade()
 
     import app.api.v1 as modules
-    api.add_namespace(modules.admin_api, "/api/v1/admin")
     api.add_namespace(modules.users_api, path='/api/v1/users')
     api.add_namespace(modules.amenities_api, path="/api/v1/amenities")
     api.add_namespace(modules.reviews_api, path="/api/v1/reviews")
--- part3/app/api/v1/__init__.py
+++ part3/app/api/v1/__init__.py
@@ -1,1 +1,5 @@
-
+from .amenities import api as amenities_api
+from .auth import api as auth_api
+from .places import api as places_api
+from .users import api as users_api
+from .reviews import api as reviews_api
